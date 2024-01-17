04529fc74108f5b63870776301230aa22c46bc10
우진 jonathan <82341563+j0ntendo@users.noreply.github.com>
2024-01-17 15:18:23 +0900

    Title: 바이러스 검사, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240117/바이러스 검사/virus-detector.cpp
+++ 240117/바이러스 검사/virus-detector.cpp
@@ -6,16 +6,24 @@ long long calc(long long array[], long long n, long long ldr, long long mbr){
     long long total = 0;
     for (int i = 0; i < n; i++){
         if ((array[i] - ldr) == 0){
-            ++total;
+            ++total;  // the leader took care of all of the customers
         } else {
             long long cust = array[i] - ldr;
             if (cust % mbr == 0){
-                total++;
-            } else{
-                total++;
+                long long members = cust/mbr;
+                for (int j = 0; j < members; j++){
+                    ++total;
+                }
+            } else {
+                long long members = cust/mbr;
+                for (int j = 0; j < members; j++){
+                    ++total;
+                }
+                if (long long remain = cust%mbr <= mbr){
+                    ++total;
+            }
             }
         }
-        
     }
         return total;
     }
